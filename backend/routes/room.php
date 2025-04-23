63861c0fa8f7028c07d5fbae2adaa1097ae7e2e2
Jeremy Caron-Labalette <jeremy.caron.labalette@gmail.com>
2025-04-23 14:24:31 +0200

    add roomimage model

--- backend/routes/room.php
+++ backend/routes/room.php
@@ -3,8 +3,12 @@
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RoomController;
+use App\Http\Controllers\RoomImageController;
 
 Route::middleware(StartSession::class)->group(function () {
     Route::apiResource('rooms', RoomController::class);
-
+    Route::apiResource('room-images', RoomImageController::class)->only([
+        'index', 'store', 'destroy'
+    ]);
 });
+
